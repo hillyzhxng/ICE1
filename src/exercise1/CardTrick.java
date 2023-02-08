45fa969b76c1523e69093d493f14d728d7d8d1c7
hilla <hilla@10.16.33.205>
2023-02-08 16:16:32 -0500

    added comment "I'm done!"

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -84,7 +84,7 @@ public class CardTrick
      */
     private static void printInfo() 
     {
-    
+    //I'm done!!
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
